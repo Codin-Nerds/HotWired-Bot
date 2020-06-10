e230924429a619b845ab996e0a0d7d82e3f866e3
ItsDrike <itsdrikeofficial@gmail.com>
2020-06-10 22:04:29 +0200

    Standardize games cog

--- cogs/games.py
+++ cogs/games.py
@@ -1,16 +1,44 @@
 import random
 
-from discord.ext import commands
+from discord.ext import Bot
+from discord.ext.commands import Cog, command, Context
+
+POSITIVE_BALL8_RESPONSES = [
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+]
+NEGATIVE_BALL8_RESPONSES = [
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+]
+UNSURE_BALL8_RESPONSES = [
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+]
 
 
-class Games(commands.Cog):
+class Games(Cog):
 
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
 
     # Commands
-    @commands.command()
-    async def roll(self, ctx, min_limit=1, max_limit=10):
+    @command()
+    async def roll(self, ctx: Context, min_limit: int = 1, max_limit: int = 10) -> None:
         """Roll a random number."""
         if max_limit - min_limit > 2:
             number = random.randint(min_limit, max_limit)
@@ -18,45 +46,20 @@ class Games(commands.Cog):
         else:
             await ctx.send('Please specify numbers with difference of **at least 2**')
 
-    @commands.command(aliases=['8ball'])
-    async def ball8(self, ctx, *, question):
+    @command(aliases=['8ball'])
+    async def ball8(self, ctx: Context, *, question: str) -> None:
         """Play 8ball."""
-        yes_responses = [
-                "Yes - definitely.",
-                "You may rely on it.",
-                "As I see it, yes.",
-                "Most likely.",
-                "Outlook good.",
-                "Yes.",
-                "Signs point to yes."
-            ]
-        responses = [
-                "It is certain.",
-                "It is decidedly so.",
-                "Without a doubt.",
-                "Reply hazy, try again.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don't count on it.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Very doubtful."
-            ]
+        reply_type = random.randint(1, 3)
 
-        def is_lucky():
-            no = random.randint(1, 3)
-            if no == 1:
-                return True
+        if reply_type == 1:  # Positive reply
+            answer = random.choice(POSITIVE_BALL8_RESPONSES)
+        elif reply_type == 2:  # Negative reply
+            answer = random.choice(NEGATIVE_BALL8_RESPONSES)
+        elif reply_type == 3:  # Unsure reply
+            answer = random.choice(UNSURE_BALL8_RESPONSES)
 
-        if is_lucky():
-            answer = random.choice(yes_responses)
-        else:
-            answer = random.choice(responses)
+        await ctx.send(f"Question: {question}\nAnswer: {answer}")
 
-        await ctx.send(f'Question : {question}\nAnswer : {answer}')
 
-def setup(client):
-    client.add_cog(Games(client))
+def setup(bot: Bot):
+    bot.add_cog(Games(bot))
