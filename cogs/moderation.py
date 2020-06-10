feb735d4ac2f2c3260cc4a471e1f3b84d546a00d
ItsDrike <itsdrikeofficial@gmail.com>
2020-06-10 21:49:30 +0200

    Standardize moderation cog

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,55 +2,58 @@ import asyncio
 
 import discord
 from cogs.utils.embedHandler import failure, info, success
-from discord.ext import commands
+from discord.ext import Bot
+from discord.ext.commands import (Cog, Context, bot_has_permissions, command,
+                                  has_permissions)
 
 
-class Moderation(commands.Cog):
+class Moderation(Cog):
+    """This cog provides moderation commands."""
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
 
-    def __init__(self, client):
-        self.client = client
-
-    @commands.command()
-    @commands.bot_has_permissions(kick_members=True)
-    @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member, *, reason="No specific reason"):
+    @command()
+    @bot_has_permissions(kick_members=True)
+    @has_permissions(kick_members=True)
+    async def kick(self, ctx: Context, member: discord.Member, *, reason: str = "N/A") -> None:
         """Kick a User."""
-        embed1 = discord.Embed(title="Infraction information", color=discord.Color.red())
-        embed1.add_field(name="Type", value="Kick")
-        embed1.add_field(name="Reason", value=reason)
-        embed1.set_thumbnail(url=member.avatar_url)
-        embed1.set_author(name=member.name, url=member.avatar_url)
-        embed1.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
+        embed = discord.Embed(title="Infraction information", color=discord.Color.red())
+        embed.add_field(name="Type", value="Kick")
+        embed.add_field(name="Reason", value=reason)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_author(name=member.name, url=member.avatar_url)
+        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
 
-        await ctx.send(embed=embed1)
-        await member.send(embed=embed1)
+        await ctx.send(embed=embed)
+        await member.send(embed=embed)
         await member.kick(reason=reason)
 
-    @commands.command()
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member, *, reason="No Reason Stated."):
+    @command()
+    @bot_has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
+    async def ban(self, ctx: Context, member: discord.Member, *, reason: str = "No Reason Stated.") -> None:
         """Ban a User."""
+        embed = discord.Embed(title="Infraction information", color=discord.Color.red())
+        embed.add_field(name="Type", value="Ban")
+        embed.add_field(name="Reason", value=reason)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_author(name=member.name, url=member.avatar_url)
+        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
 
-        embed1 = discord.Embed(title="Infraction information", color=discord.Color.red())
-        embed1.add_field(name="Type", value="Ban")
-        embed1.add_field(name="Reason", value=reason)
-        embed1.set_thumbnail(url=member.avatar_url)
-        embed1.set_author(name=member.name, url=member.avatar_url)
-        embed1.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
-
-        await ctx.send(embed=embed1)
-        await member.send(embed=embed1)
+        await ctx.send(embed=embed)
+        await member.send(embed=embed)
         await member.ban(reason=reason)
 
-    @commands.command()
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.has_permissions(ban_members=True)
-    async def unban(ctx, *, member):
+    @command()
+    @bot_has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
+    async def unban(ctx: Context, *, member: str) -> None:
         """Unban a User."""
+        # TODO: Use custom converter for `member`
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
 
+        # TODO: There might be a better way to handle this
         for ban_entry in banned_users:
             user = ban_entry.user
 
@@ -59,12 +62,13 @@ class Moderation(commands.Cog):
                 await ctx.send(f'Unbanned **{user.name}#{user.discriminator}**')
                 return
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_messages=True)
-    @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx, amount: int):
+    @command()
+    @bot_has_permissions(manage_messages=True)
+    @has_permissions(manage_messages=True)
+    async def clear(self, ctx: Context, amount: int) -> None:
         """Clear specified number of messages."""
 
+        # TODO: Check if this condition is necessary
         if amount is not None:
             await ctx.channel.purge(limit=amount+1)
             await ctx.send('**Messages cleared** ' + ctx.message.author.mention)
@@ -73,15 +77,16 @@ class Moderation(commands.Cog):
         else:
             await ctx.send('please specify the number of messages to clear')
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_roles=True)
-    @commands.has_permissions(manage_roles=True, manage_messages=True)
-    async def promote(self, ctx, member: discord.Member, role: discord.Role):
+    @command()
+    @bot_has_permissions(manage_roles=True)
+    @has_permissions(manage_roles=True, manage_messages=True)
+    async def promote(self, ctx: Context, member: discord.Member, role: discord.Role) -> None:
         """Promote member to role."""
+        # TODO: A custom check can handle this
         if role >= ctx.author.top_role:
             await ctx.send(embed=failure("Role needs to be below you in hierarchy."))
             return
-        elif role in member.roles:
+        if role in member.roles:
             await ctx.send(embed=failure(f"{member.mention} already has role {role.mention}!"))
             return
 
@@ -92,8 +97,8 @@ class Moderation(commands.Cog):
         dm_embed = info(
             (
                 f"You are now promoted to role **{role.name}** in our community.\n"
-                f"`'With great power comes great responsibility'`\n"
-                f"Be active and keep the community safe."
+                "`'With great power comes great responsibility'`\n"
+                "Be active and keep the community safe."
             ),
             ctx.me,
             "Congratulations!"
@@ -103,5 +108,5 @@ class Moderation(commands.Cog):
         await member.send(embed=dm_embed)
 
 
-def setup(bot):
+def setup(bot: Bot) -> None:
     bot.add_cog(Moderation(bot))
