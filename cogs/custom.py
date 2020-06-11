717daf95f60154b2fd8c7e942dfb32c6a1f4195b
janaSunrise <warriordefenderz@gmail.com>
2020-06-11 23:37:02 +0530

    add some tools and update the old

--- cogs/custom.py
+++ cogs/custom.py
@@ -4,6 +4,8 @@ import discord
 from cogs.utils.embedHandler import error_embed, info
 from discord.ext import commands
 
+from .utils import constants
+
 
 class Custom(commands.Cog):
 
@@ -13,7 +15,7 @@ class Custom(commands.Cog):
     # Commands
     @commands.command()
     async def hello(self, ctx):
-        await ctx.send('Hey there Buddy!')
+        await ctx.send(f'Hey there Buddy! My Creator is {constants.creator}')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
@@ -24,6 +26,25 @@ class Custom(commands.Cog):
         end = time.perf_counter()
         duration = (end - start) * 1000
         await message.edit(embed=info(f":ping_pong: {duration:.2f}ms", ctx.me, "Pong!"))
+
+    @commands.command(aliases=("poll",))
+    async def vote(self, ctx: commands.Context, title: str, *options: str) -> None:
+        """
+        Build a quick voting poll with matching reactions with the provided options.
+        A maximum of 20 options can be provided, as Discord supports a max of 20
+        reactions on a single message.
+        """
+        if len(options) < 2:
+            raise commands.BadArgument("Please provide at least 2 options.")
+        if len(options) > 20:
+            raise commands.BadArgument("I can only handle 20 options!")
+
+        codepoint_start = 127462  # represents "regional_indicator_a" unicode value
+        options = {chr(i): f"{chr(i)} - {v}" for i, v in enumerate(options, start=codepoint_start)}
+        embed = discord.Embed(title=title, description="\n".join(options.values()))
+        message = await ctx.send(embed=embed)
+        for reaction in options:
+            await message.add_reaction(reaction)
 
     @commands.command(aliases=['asking'])
     async def howtoask(self, ctx):
--- cogs/tools.py
+++ cogs/tools.py
@@ -6,6 +6,10 @@ from discord.ext import commands
 from utils.mathscrape import get_math_results
 from utils.wolframscrape import get_wolfram_data
 
+import unicodedata
+from typing import Tuple
+import re
+
 
 class Tools(commands.Cog):
 
@@ -131,6 +135,62 @@ class Tools(commands.Cog):
         embed.add_field(name="**❯❯ Result**", value=data, inline=False)
 
         embed.set_footer(text=f'Invoked by {str(ctx.message.author)}')
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+        """Shows you information about a number of characters. Only up to 25 characters at a time."""
+        def to_string(c):
+            digit = f'{ord(c):x}'
+            name = unicodedata.name(c, 'Name not found.')
+            return f'`\\U{digit:>08}`: {name} - {c} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{digit}>'
+
+        msg = '\n'.join(map(to_string, characters))
+        if len(msg) > 2000:
+            return await ctx.send('Output too long to display.')
+        await ctx.send(msg)
+
+    @commands.command()
+    async def charinfo_uni(self, ctx: commands.Context, *, characters: str) -> None:
+        """Shows you information on up to 25 unicode characters."""
+        match = re.match(r"<(a?):(\w+):(\d+)>", characters)
+        if match:
+            embed = discord.Embed(
+                title="Non-Character Detected",
+                description=(
+                    "Only unicode characters can be processed, but a custom Discord emoji "
+                    "was found. Please remove it and try again."
+                )
+            )
+            embed.colour = discord.Colour.red()
+            await ctx.send(embed=embed)
+            return
+
+        if len(characters) > 25:
+            embed = discord.Embed(title=f"Too many characters ({len(characters)}/25)")
+            embed.colour = discord.Colour.red()
+            await ctx.send(embed=embed)
+            return
+
+        def get_info(char: str) -> Tuple[str, str]:
+            digit = f"{ord(char):x}"
+            if len(digit) <= 4:
+                u_code = f"\\u{digit:>04}"
+            else:
+                u_code = f"\\U{digit:>08}"
+            url = f"https://www.compart.com/en/unicode/U+{digit:>04}"
+            name = f"[{unicodedata.name(char, '')}]({url})"
+            info = f"`{u_code.ljust(10)}`: {name} - {char}"
+            return info, u_code
+
+        charlist, rawlist = zip(*(get_info(c) for c in characters))
+
+        embed = discord.Embed(description="\n".join(charlist))
+        embed.set_author(name="Character Info")
+
+        if len(characters) > 1:
+            embed.add_field(name='Raw', value=f"`{''.join(rawlist)}`", inline=False)
 
         await ctx.send(embed=embed)
 
