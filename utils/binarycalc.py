2b3afdc3032051f72238f6b30dbcac7c8d9c2ba4
ItsDrike <itsdrikeofficial@gmail.com>
2020-06-14 14:38:24 +0200

    Further standardization of binarycalc.py

--- utils/binarycalc.py
+++ utils/binarycalc.py
@@ -6,31 +6,21 @@ def to_binary(number: int) -> str:
     return bin(number).replace("0b", "")
 
 
-def binary_to_decimal(number: str) -> int:
-    """Convert any binary string into integer."""
-    return int(number, 2)
+def binaryop(num1: str, num2: str, operator: t.Literal["+", "-", "*", "/"]) -> str:
+    try:
+        num1 = int(num1, 2)
+        num2 = int(num2, 2)
+    except ValueError:
+        return "Invalid Binary Number"
 
+    operations = {
+        "+": lambda n1, n2: to_binary(n1 + n2),
+        "-": lambda n1, n2: to_binary(n1 - n2),
+        "*": lambda n1, n2: to_binary(n1 * n2),
+        "/": lambda n1, n2: to_binary(n1 / n2),
+    }
 
-def is_binary(*args: t.List[str]) -> bool:
-    """Check if all passed strings only consists of 1s or 0s."""
-    all("1" == char or "0" == char for string in args for char in string)
-
-
-def binaryop(num1: str, num2: str, op: t.Literal["+", "-", "*", "/"]) -> str:
-    if is_binary(num1, num2):
-        num1 = binary_to_decimal(num1)
-        num2 = binary_to_decimal(num2)
-
-        if op == "+":
-            return to_binary(num1 + num2)
-        elif op == "-":
-            return to_binary(num1 - num2)
-        elif op == "*":
-            return to_binary(num1 * num2)
-        elif op == "/":
-            try:
-                return to_binary(num1 / num2)
-            except ZeroDivisionError:
-                return "N/A (ZERO DIVISION)"
-        else:
-            return "Invalid Binary Number"
+    try:
+        return operations[operator](num1, num2)
+    except ZeroDivisionError:
+        return "N/A (ZERO DIVISION)"
