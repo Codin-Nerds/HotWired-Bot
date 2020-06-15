8d651c1620a029e3a4952747ec198171c36b06bc
janaSunrise <warriordefenderz@gmail.com>
2020-06-15 18:43:51 +0530

    merge master

--- README.md
+++ README.md
@@ -2,6 +2,41 @@
 HotWired - Discord Bot
 =======================
 
+[![mit](https://img.shields.io/badge/Licensed%20under-MIT-red.svg?style=flat-square)](./LICENSE)
+[![Build Status](https://travis-ci.com/The-Codin-Hole/HotWired-Bot.svg?branch=master)](https://travis-ci.com/The-Codin-Hole/HotWired-Bot)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python%203.8-ffe900.svg?longCache=true&style=flat-square&colorB=00a1ff&logo=python&logoColor=88889e)](https://www.python.org/)
+[![made-with-discord.py](https://img.shields.io/badge/Using-discord.py-ffde57.svg?longCache=true&style=flat-square&colorB=4584b6&logo=discord&logoColor=7289DA)](https://github.com/Rapptz/discord.py)
 [![Discord](https://img.shields.io/static/v1?label=The%20Codin'%20Hole&logo=discord&message=%3E200%20members&color=%237289DA&logoColor=white)](https://discord.gg/vP26dCy)
 
-This bot is Custom made For the codin' Hole Discord server
+This bot is a Heavy Coded Yet lightWeight, reliable, smooth, automatic worker
+that does everything you need. It's a One stop Shop for everything.
+
+Try this bot Today!
+
+Features
+-------
+
+* Perfect Event management
+* Perfect Moderation for Your server
+* No Paid Options, Everything for Free!
+* Extra Options and stats for Server owners or admins.
+* No web dashboard, management made easy through discord
+* Inbuilt Compiler for programmers, **160+ Languages**!
+* Inbuilt documentation and reference support, No more leaving Discord. **Yay!**
+* A Fast repl for Python inside the bot.
+* Study and Study Helper Commands
+* A perfectly crafted arsenal for the programmers
+* Another Perfectly crafted Arsenal for The hackers
+* Some Fun Commands Too!
+* Bored? Play Games through our bot.
+* Get a coding Sandbox inside Discord.
+* Make your Custom commands!
+* Various inbuilt custom commands From the Creator.
+* More Features Coming Soon. Stay Tuned!
+
+Support
+-------
+
+The Bot Creator Team has **24x7** Support for the Bot,
+Just join The Server and give your valuable suggestion,
+feedback or the Complaint, We will Look Into it ASAP!
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,14 +1,27 @@
 import asyncio
 from collections import Counter
-from cogs.utils.embedHandler import failure, info, success
 import typing as t
 from .utils.formats import plural
+import textwrap
 
 import discord
-from discord.ext import commands
+from discord import Color, Embed
+from discord.ext.commands import (
+    BadArgument,
+    Bot,
+    Cog,
+    Context,
+    Converter,
+    bot_has_permissions,
+    command,
+    guild_only,
+    Greedy,
+    has_permissions,
+    MemberConverter,
+)
 
 
-def can_execute_action(ctx: commands.Context, user: discord.User, target: discord.User) -> bool:
+def can_execute_action(ctx: Context, user: discord.User, target: discord.User) -> bool:
     return user.id == ctx.bot.owner_id or \
            user == ctx.guild.owner or \
            user.top_role > target.top_role
@@ -18,7 +31,7 @@ class MemberNotFound(Exception):
     pass
 
 
-async def resolve_member(self, guild: discord.Guild, member_id: int):
+async def resolve_member(self, guild: discord.Guild, member_id: int) -> discord.User:
     user = guild.get_member(member_id)
     if user is None:
         if guild.chunked:
@@ -31,76 +44,77 @@ async def resolve_member(self, guild: discord.Guild, member_id: int):
 
 
 # TODO : Clean everything up
-class Moderation(commands.Cog):
+class Moderation(Cog):
 
     def __init__(self, client: discord.Bot):
         self.client = client
 
-    @commands.command()
-    @commands.bot_has_permissions(kick_members=True)
-    @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No specific reason") -> None:
+    @command()
+    @bot_has_permissions(kick_members=True)
+    @has_permissions(kick_members=True)
+    async def kick(self, ctx: Context, member: discord.Member, *, reason: str = "No specific reason") -> None:
         """Kick a User."""
-        embed1 = discord.Embed(title="Infraction information", color=discord.Color.red())
-        embed1.add_field(name="Type", value="Kick")
-        embed1.add_field(name="Reason", value=reason)
-        embed1.set_thumbnail(url=member.avatar_url)
-        embed1.set_author(name=member.name, url=member.avatar_url)
-        embed1.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
 
-        await ctx.send(embed=embed1)
-        await member.send(embed=embed1)
+        embed = discord.Embed(title="Infraction information", color=discord.Color.red())
+        embed.add_field(name="Type", value="Kick")
+        embed.add_field(name="Reason", value=reason)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_author(name=member.name, url=member.avatar_url)
+        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
+
+        await ctx.send(embed=embed)
+        await member.send(embed=embed)
         await member.kick(reason=reason)
 
-    @commands.command()
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = "No Reason Stated.") -> None:
+    @command()
+    @bot_has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
+    async def ban(self, ctx: Context, member: discord.Member, *, reason: str = "No Reason Stated.") -> None:
         """Ban a User."""
 
-        embed1 = discord.Embed(title="Infraction information", color=discord.Color.red())
-        embed1.add_field(name="Type", value="Ban")
-        embed1.add_field(name="Reason", value=reason)
-        embed1.set_thumbnail(url=member.avatar_url)
-        embed1.set_author(name=member.name, url=member.avatar_url)
-        embed1.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
+        embed = discord.Embed(title="Infraction information", color=discord.Color.red())
+        embed.add_field(name="Type", value="Ban")
+        embed.add_field(name="Reason", value=reason)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_author(name=member.name, url=member.avatar_url)
+        embed.set_footer(text=member.guild.name, icon_url=member.guild.icon_url)
 
-        await ctx.send(embed=embed1)
-        await member.send(embed=embed1)
+        await ctx.send(embed=embed)
+        await member.send(embed=embed)
         await member.ban(reason=reason)
 
-    class ActionReason(commands.Converter):
-        async def convert(self, ctx: commands.Context, argument: str) -> str:
+    class ActionReason(Converter):
+        async def convert(self, ctx: Context, argument: str) -> str:
             ret = f'{ctx.author} (ID: {ctx.author.id}): {argument}'
 
             if len(ret) > 512:
                 reason_max = 512 - len(ret) + len(argument)
-                raise commands.BadArgument(f'Reason is too long ({len(argument)}/{reason_max})')
+                raise BadArgument(f'Reason is too long ({len(argument)}/{reason_max})')
             return ret
 
-    class MemberID(commands.Converter):
-        async def convert(self, ctx: commands.Context, argument: str) -> t.Union[str, type]:
+    class MemberID(Converter):
+        async def convert(self, ctx: Context, argument: str) -> t.Union[str, type]:
             try:
-                member = await commands.MemberConverter().convert(ctx, argument)
-            except commands.BadArgument:
+                member = await MemberConverter().convert(ctx, argument)
+            except BadArgument:
                 try:
                     member_id = int(argument, base=10)
                     member = await resolve_member(ctx.guild, member_id)
                 except ValueError:
-                    raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+                    raise BadArgument(f"{argument} is not a valid member or member ID.") from None
                 except MemberNotFound:
                     # hackban case
                     return type('_Hackban', (), {'id': member_id, '__str__': lambda s: f'Member ID {s.id}'})()
 
             if not can_execute_action(ctx, ctx.author, member):
-                raise commands.BadArgument('You cannot do this action on this user due to role hierarchy.')
+                raise BadArgument('You cannot do this action on this user due to role hierarchy.')
             return member
 
-    @commands.command()
-    @commands.guild_only()
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.has_permissions(ban_members=True)
-    async def multiban(self, ctx: commands.Context, members: commands.Greedy[MemberID], *, reason: ActionReason = None) -> None:
+    @command()
+    @guild_only()
+    @bot_has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
+    async def multiban(self, ctx: Context, members: Greedy[MemberID], *, reason: ActionReason = None) -> None:
         """
         Bans multiple members from the server.
 
@@ -129,66 +143,79 @@ class Moderation(commands.Cog):
 
         await ctx.send(f'Banned {total_members - failed}/{total_members} members.')
 
-    @commands.command()
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx: commands.Context, *, member: str) -> None:
+    @command()
+    @bot_has_permissions(ban_members=True)
+    @has_permissions(ban_members=True)
+    async def unban(self, ctx: Context, *, member: str) -> None:
         """Unban a User."""
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
 
+        # TODO: Use custom converter for `member`
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split("#")
+
+        # TODO: There might be a better way to handle this
         for ban_entry in banned_users:
             user = ban_entry.user
 
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned **{user.name}#{user.discriminator}**')
+                await ctx.send(f"Unbanned **{user.name}#{user.discriminator}**")
                 return
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_messages=True)
-    @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx: commands.Context, amount: int) -> None:
+    @command()
+    @bot_has_permissions(manage_messages=True)
+    @has_permissions(manage_messages=True)
+    async def clear(self, ctx: Context, amount: int) -> None:
         """Clear specified number of messages."""
 
+        # TODO: Check if this condition is necessary
         if amount is not None:
             await ctx.channel.purge(limit=amount + 1)
-            await ctx.send('**Messages cleared** ' + ctx.message.author.mention)
+
+            # TODO: This message might be getting in the way,
+            # purpose of cleaning is to remove messages, not add more of them
+            await ctx.send("**Messages cleared** " + ctx.message.author.mention)
             await asyncio.sleep(2.5)
             await ctx.channel.purge(limit=1)
         else:
-            await ctx.send('please specify the number of messages to clear')
+            await ctx.send("please specify the number of messages to clear")
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_roles=True)
-    @commands.has_permissions(manage_roles=True, manage_messages=True)
-    async def promote(self, ctx: commands.Context, member: discord.Member, role: discord.Role) -> None:
+    @command()
+    @bot_has_permissions(manage_roles=True)
+    @has_permissions(manage_roles=True, manage_messages=True)
+    async def promote(self, ctx: Context, member: discord.Member, role: discord.Role) -> None:
         """Promote member to role."""
+
+        # TODO: A custom check can handle this
         if role >= ctx.author.top_role:
-            await ctx.send(embed=failure("Role needs to be below you in hierarchy."))
+            embed = Embed(title="Error", description="Role needs to be below you in hierarchy.", color=Color.red(),)
+            await ctx.send(embed=embed)
             return
-        elif role in member.roles:
-            await ctx.send(embed=failure(f"{member.mention} already has role {role.mention}!"))
+        if role in member.roles:
+            embed = Embed(title="Error", description=f"{member.mention} already had role {role.mention}!", color=Color.red(),)
+            await ctx.send(embed=embed)
             return
 
         await member.add_roles(role)
 
-        await ctx.send(embed=success(f"{member.mention} is promoted to {role.mention}", ctx.me), delete_after=5)
+        embed = Embed(title="Promotion!", description=f"{member.mention} has been promoted to {role.mention}!", color=Color.green(),)
+        await ctx.send(embed=embed, delete_after=5)
 
-        dm_embed = info(
-            (
-                f"You are now promoted to role **{role.name}** in our community.\n"
-                f"`'With great power comes great responsibility'`\n"
-                f"Be active and keep the community safe."
+        dm_embed = Embed(
+            title="Congratulations!",
+            description=textwrap.dedent(
+                f"""
+                You have been promoted to **{role.name}** in our community.
+                `'With great power comes great responsibility'`
+                Be active and keep the community safe
+                """
             ),
-            ctx.me,
-            "Congratulations!"
+            color=Color.green(),
         )
-
-        dm_embed.set_footer(text="Promotion Command.")
+        await dm_embed.set_footer(text=f"Server: {ctx.guild.name}")
         await member.send(embed=dm_embed)
 
-    async def _basic_cleanup_strategy(self, ctx: commands.Context, search: int) -> dict:
+    async def _basic_cleanup_strategy(self, ctx: Context, search: int) -> dict:
         count = 0
         async for msg in ctx.history(limit=search, before=ctx.message):
             if msg.author == ctx.me:
@@ -196,7 +223,7 @@ class Moderation(commands.Cog):
                 count += 1
         return {'Bot': count}
 
-    async def _complex_cleanup_strategy(self, ctx: commands.Context, search: int) -> Counter:
+    async def _complex_cleanup_strategy(self, ctx: Context, search: int) -> Counter:
         prefixes = tuple(self.client.get_guild_prefixes(ctx.guild))  # thanks startswith
 
         def check(member: discord.Bot) -> bool:
@@ -205,10 +232,10 @@ class Moderation(commands.Cog):
         deleted = await ctx.channel.purge(limit=search, check=check, before=ctx.message)
         return Counter(m.author.display_name for m in deleted)
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_messages=True)
-    @commands.has_permissions(manage_messages=True)
-    async def cleanup(self, ctx: commands.Context, search: int = 100) -> None:
+    @command()
+    @bot_has_permissions(manage_messages=True)
+    @has_permissions(manage_messages=True)
+    async def cleanup(self, ctx: Context, search: int = 100) -> None:
         """Cleans up the bot's messages from the channel."""
 
         strategy = self._basic_cleanup_strategy
@@ -228,5 +255,5 @@ class Moderation(commands.Cog):
         await ctx.send('\n'.join(messages), delete_after=10)
 
 
-def setup(bot: discord.Bot) -> None:
+def setup(bot: Bot) -> None:
     bot.add_cog(Moderation(bot))
