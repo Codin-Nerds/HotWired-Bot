7bce55419895e0475a236e2dd7091dfb133ebea1
Warrior Defend <56873669+janaSunrise@users.noreply.github.com>
2020-06-19 10:34:22 +0530

    Update cogs/events.py
    Co-authored-by: ItsDrike <itsdrikeofficial@gmail.com>

--- cogs/events.py
+++ cogs/events.py
@@ -47,8 +47,7 @@ class Custom(Cog):
                 **HotWired** is a multi purpose discord bot that has Moderation commands, Fun commands,
                 Music commands and many more!.
                 The bot is still in dev so you can expect more commands and features.To get a list of commands ,
-                please use **{PREFIX}help**
-            """
+                """
             ),
             color=0x2F3136,
         )
