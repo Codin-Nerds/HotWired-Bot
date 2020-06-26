98deef4ad48f0bbd388fc528c4ec163f0ae970f1
ItsDrike <itsdrikeofficial@gmail.com>
2020-06-26 23:28:50 +0200

    Remove embed handler and members.py - These files are only being used in commands, keeping them there is good enough

--- bot/cogs/commands.py
+++ bot/cogs/commands.py
@@ -3,12 +3,16 @@ import textwrap
 import typing as t
 from collections import Counter
 
-from discord import Color, Embed, Member
+from discord import Color, Embed, Member, Status, User, Guild, ActivityType
 from discord.ext.commands import Bot, Cog, Context, command
 
-from bot import constants
 
-from .utils.embed_handler import status_embed
+STATUSES = {
+    Status.dnd: "DND 🔴",
+    Status.online: "ONLINE \U0001F7E2",
+    Status.idle: "IDLE 🌙",
+    Status.offline: "OFFLINE 💀",
+}
 
 
 class Commands(Cog):
@@ -38,98 +42,133 @@ class Commands(Cog):
 
         await ctx.send(embed=embed)
 
-    @command()
-    async def status(self, ctx: Context, member: t.Optional[Member] = None) -> None:
-        """Returns the status of a member."""
-        if member is None:
-            member = ctx.author
-
-        if member.id in constants.owner_ids:
-            embed = status_embed(member, description="None")
-        else:
-            embed = status_embed(member)
-
-        await ctx.send(embed=embed)
-
     @command(aliases=["server"])
     async def serverinfo(self, ctx: Context) -> None:
         """Get information about the server."""
-
-        embed = Embed(colour=Color.gold())
-        embed.title = f"{ctx.guild.name}'s stats and information."
-        embed.description = ctx.guild.description if ctx.guild.description else None
-        embed.add_field(
-            name="__**General information:**__",
-            value=textwrap.dedent(
-                f"""
-                **Owner:** {ctx.guild.owner}
-                **Created at:** {datetime.datetime.strftime(ctx.guild.created_at, "%A %d %B %Y at %H:%M")}
-                **Members:** {ctx.guild.member_count}
-                **Nitro Tier:** {ctx.guild.premium_tier} | **Boosters:** {ctx.guild.premium_subscription_count}
-                **File Size:** {round(ctx.guild.filesize_limit / 1048576)} MB
-                **Bitrate:** {round(ctx.guild.bitrate_limit / 1000)} kbps
-                **Emoji:** {ctx.guild.emoji_limit}
-                """
-            ),
-        )
-
-        embed.add_field(
-            name="__**Channels:**__",
-            value=textwrap.dedent(
-                f"""
-                **AFK timeout:** {int(ctx.guild.afk_timeout / 60)}m | **AFK channel:** {ctx.guild.afk_channel}
-                **Text channels:** {len(ctx.guild.text_channels)} | **Voice channels:** {len(ctx.guild.voice_channels)}
-                """
-            ),
-            inline=False,
-        )
-
-        embed.set_footer(text=f"ID: {ctx.guild.id}")
-
-        await ctx.send(embed=embed)
-        return
+        await ctx.send(embed=self.get_server_embed(ctx.guild))
 
     @command(aliases=["user"])
-    async def userinfo(self, ctx: Context, *, member: t.Optional[Member] = None) -> None:
+    async def userinfo(self, ctx: Context, user: t.Optional[t.Union[Member, User]] = None) -> None:
         """
         Get information about you, or a specified member.
 
-        `member`: The member to get information from. Can be a Mention, Name or ID.
+        `user`: The user to get information from. Can be a Mention, Name or ID.
         """
+        if not user:
+            user = ctx.author
 
-        if not member:
-            member = ctx.author
+        await ctx.send(embed=self.get_user_embed(user))
 
-        embed = Embed(colour=Color.gold())
-        embed.title = f"{member}'s stats and information."
+    def get_user_embed(self, user: t.Union[User, Member]) -> Embed:
+        """Create an embed with detailed info about given user"""
+        embed = Embed(
+            title=f"{user}'s stats and information.",
+            color=Color.blue()
+        )
+
+        created_time = datetime.datetime.strftime(user.created_at, "%A %d %B %Y at %H:%M")
+
+        user_info = textwrap.dedent(
+            f"""
+            Mention: {user.mention}
+            Date Created: {created_time}
+            ID: {user.id}
+            """
+        )
+
+        if isinstance(user, Member):
+            member_info = []
+
+            if user.nick:
+                member_info.append(f"Nickname: {user.nick}")
+
+            joined_time = datetime.datetime.strftime(user.joined_at, "%A %d %B %Y at %H:%M")
+            member_info.append(f"Joined server: {joined_time}")
+
+            member_info.append("")
+
+            try:
+                member_info.append(f"Status: {STATUSES[user.status]}")
+            except KeyError:
+                member_info.append("Status: N/A")
+
+            if user.status == Status.offline:
+                member_info.append("Device: :no_entry:")
+            elif user.is_on_mobile():
+                member_info.append("Device: Phone :iphone:")
+            else:
+                member_info.append("Device: PC :desktop:")
+
+            if not user.activity:
+                member_info.append("Activity: N/A")
+            elif user.activity.type != ActivityType.custom:
+                member_info.append(f"Activity: {user.activity.type.name} {user.activity.name}")
+            else:
+                member_info.append(f"Activity: {user.activity.name}")
+
+            member_info.append("")
+
+            roles = " ".join(role.mention for role in user.roles[1:])
+            member_info.append(f"Top Role: {user.top_role.mention}")
+            member_info.append(f"All Roles: {roles}")
+        else:
+            member_info = ["No server related info, this user is not a member of this server."]
+
         embed.add_field(
-            name="__**General information:**__",
+            name="❯❯ General information",
+            value=user_info,
+            inline=False
+        )
+
+        embed.add_field(
+            name="❯❯ Server-related information",
+            value="\n".join(member_info),
+            inline=False
+        )
+
+        embed.set_thumbnail(url=user.avatar_url_as(format="png"))
+
+        return embed
+
+    def get_server_embed(self, guild: Guild) -> Embed:
+        embed = Embed(
+            title="Serer's stats and information.",
+            description=guild.description if guild.description else None,
+            color=Color.gold()
+        )
+
+        embed.add_field(
+            name="❯❯ General information",
             value=textwrap.dedent(
                 f"""
-                **Discord Name:** {member}
-                **Created at:** {datetime.datetime.strftime(member.created_at, "%A %d %B %Y at %H:%M")}
+                Name: {guild.name}
+                Created at: {datetime.datetime.strftime(guild.created_at, "%A %d %B %Y at %H:%M")}
+                Members: {guild.member_count}
+                Owner: {guild.owner}
+                Nitro Tier: {guild.premium_tier} | Boosters: {guild.premium_subscription_count}
+
+                File Size: {round(guild.filesize_limit / 1048576)} MB
+                Bitrate: {round(guild.bitrate_limit / 1000)} kbps
+                Emoji: {guild.emoji_limit}
+                """
+            ),
+        )
+        embed.add_field(
+            name="❯❯ Channels",
+            value=textwrap.dedent(
+                f"""
+                Text channels: {len(guild.text_channels)}
+                Voice channels: {len(guild.voice_channels)}
+                AFK timeout: {round(guild.afk_timeout / 60)}m | AFK channel: {guild.afk_channel}
                 """
             ),
             inline=False,
         )
 
-        embed.add_field(
-            name="__**Server-related information:**__",
-            value=textwrap.dedent(
-                f"""
-                **Nickname:** {member.nick}
-                **Joined server:** {datetime.datetime.strftime(member.joined_at, "%A %d %B %Y at %H:%M")}
-                **Top role:** {member.top_role.mention}
-                """
-            ),
-            inline=False,
-        )
+        embed.set_thumbnail(url=guild.icon_url)
+        embed.set_footer(text=f"ID: {guild.id}")
 
-        embed.set_thumbnail(url=member.avatar_url_as(format="png"))
-        embed.set_footer(text=f"ID: {member.id}")
-
-        await ctx.send(embed=embed)
-        return
+        return embed
 
 
 def setup(bot: Bot) -> None:
