d520071e937ff488dbc71e4dccada1badbad858f
janaSunrise <warriordefenderz@gmail.com>
2020-07-12 23:40:53 +0530

    Add animes features

--- cogs/search.py
+++ cogs/search.py
@@ -1,12 +1,14 @@
 import re
 from typing import List
 from .utils import constants
-import textwrap
+import json
+import datetime
 
 import aiohttp
 import html2text
-from discord import Embed, utils
-from discord.ext.commands import Bot, Cog, Context, command
+from discord import Embed, utils, Color
+from discord.ext.commands import Bot, Cog, Context, command, bot_has_permissions
+import typing as t
 
 from .search_utils import searchexceptions
 from .search_utils.regex import filter_words
@@ -19,6 +21,7 @@ class Search(Cog, name="Basic"):
         # Main Stuff
         self.bot = bot
         self.emoji = "\U0001F50D"
+        self.url = "https://graphql.anilist.co"
 
         # Markdown converter
         self.tomd = html2text.HTML2Text()
@@ -113,125 +116,277 @@ class Search(Cog, name="Basic"):
             return
         await self._basic_search(ctx, query, category)
 
+    async def _search_anime_manga(self, ctx: Context, cmd: str, entered_title: str) -> t.Union[tuple, None]:
+
+        media_status_to_string = {
+            "FINISHED": "Finished",
+            "RELEASING": "Releasing",
+            "NOT_YET_RELEASED": "Not yet released",
+            "CANCELLED": "Cancelled",
+        }
+
+        variables = {"search": entered_title, "page": 1, "type": cmd}
+
+        data = (await self._request(constants.SEARCH_ANIME_MANGA_QUERY, variables))["data"]["Page"]["media"]
+
+        if data is not None and len(data) > 0:
+
+            embeds = []
+
+            for anime_manga in data:
+                link = f"https://anilist.co/{cmd.lower()}/{anime_manga['id']}"
+                description = anime_manga["description"]
+                title = anime_manga["title"]["english"] or anime_manga["title"]["romaji"]
+                if anime_manga.get("nextAiringEpisode"):
+                    seconds = anime_manga["nextAiringEpisode"]["timeUntilAiring"]
+                    time_left = str(datetime.timedelta(seconds=seconds))
+                else:
+                    time_left = "Never"
+
+                external_links = ""
+                for i in range(0, len(anime_manga["externalLinks"])):
+                    ext_link = anime_manga["externalLinks"][i]
+                    external_links += f"[{ext_link['site']}]({ext_link['url']}), "
+                    if i + 1 == len(anime_manga["externalLinks"]):
+                        external_links = external_links[:-2]
+
+                embed = Embed(
+                    title=title,
+                    color=Color.green(),
+                    description=self.description_parser(description)
+                )
+                embed.url = link
+                embed.set_thumbnail(url=anime_manga["coverImage"]["medium"])
+                embed.add_field(name="Score", value=anime_manga.get("averageScore", "N/A"))
+
+                if cmd == "ANIME":
+                    embed.add_field(name="Episodes", value=anime_manga.get("episodes", "N/A"))
+                    embed.set_footer(
+                        text="Status : " + media_status_to_string[anime_manga["status"]] + ", Next episode : " + time_left + ", Powered by Anilist"
+                    )
+                else:
+                    embed.add_field(name="Chapters", value=anime_manga.get("chapters", "N/A"))
+                    embed.set_footer(text="Status : " + media_status_to_string.get(anime_manga.get("status"), "N/A") + ", Powered by Anilist")
+
+                if external_links:
+                    embed.add_field(name="Streaming and/or Info sites", value=external_links)
+
+                if anime_manga["bannerImage"]:
+                    embed.set_image(url=anime_manga["bannerImage"])
+                embed.add_field(
+                    name="You can find out more",
+                    value=f"[Anilist]({link}), [MAL](https://myanimelist.net/{cmd.lower()}/{anime_manga['idMal']}), Kitsu (Soon™)"
+                )
+                embeds.append(embed)
+
+            return embeds, data
+
+        else:
+            return None
+
+    async def _search_character(self, ctx: Context, entered_title: str) -> t.Union[tuple, None]:
+
+        variables = {"search": entered_title, "page": 1}
+
+        data = (await self._request(constants.SEARCH_CHARACTER_QUERY, variables))["data"]["Page"]["characters"]
+
+        if data is not None and len(data) > 0:
+            embeds = []
+
+            for character in data:
+                link = f"https://anilist.co/character/{character['id']}"
+                character_anime = [f'[{anime["title"]["userPreferred"]}]({"https://anilist.co/anime/" + str(anime["id"])})'
+                                   for anime in character["media"]["nodes"] if anime["type"] == "ANIME"]
+                character_manga = [f'[{manga["title"]["userPreferred"]}]({"https://anilist.co/manga/" + str(manga["id"])})'
+                                   for manga in character["media"]["nodes"] if manga["type"] == "MANGA"]
+
+                embed = Embed(
+                    title=self.format_name(character["name"]["first"], character["name"]["last"]),
+                    color=Color.blue(),
+                    description=self.description_parser(character["description"])
+                )
+                embed.url = link
+                embed.set_thumbnail(url=character["image"]["large"])
+                if len(character_anime) > 0:
+                    embed.add_field(name="Anime", value="\n".join(self.list_maximum(character_anime)))
+                if len(character_manga) > 0:
+                    embed.add_field(name="Manga", value="\n".join(self.list_maximum(character_manga)))
+                embed.set_footer(text="Powered by Anilist")
+                embeds.append(embed)
+
+            return embeds, data
+
+        else:
+            return None
+
     @command()
-    async def anime(self, ctx: Context, *, query: str) -> None:
-        """Look up anime information."""
-        base = "https://kitsu.io/api/edge/"
+    @bot_has_permissions(embed_links=True, add_reactions=True)
+    async def anime(self, ctx: Context, *, entered_title: str) -> None:
+        """Searches for anime."""
 
-        # Handling
-        async with ctx.typing():
-            async with aiohttp.ClientSession() as session:
-                async with session.get(base + "anime", params={"filter[text]": query}) as resp:
-                    resp = await resp.json()
-                    resp = resp["data"]
+        try:
+            cmd = "ANIME"
+            embeds, data = await self._search_anime_manga(ctx, cmd, entered_title)
 
-                    query = utils.escape_mentions(query)
-                    query = utils.escape_markdown(query)
+            if embeds is not None:
+                # TODO: add pagination
+                await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
+            else:
+                await ctx.send("No anime was found or there was an error in the process")
 
-                    if not resp:
-                        await ctx.send(f"No results for `{query}`.")
-                        return
+        except TypeError:
+            await ctx.send("No anime was found or there was an error in the process")
 
-                    anime = resp[0]
-                    title = f'{anime["attributes"]["canonicalTitle"]}'
-                    anime_id = anime["id"]
-                    url = f"https://kitsu.io/anime/{anime_id}"
-                    thing = "" if not anime["attributes"]["endDate"] else f' to {anime["attributes"]["endDate"]}'
+    @command()
+    @bot_has_permissions(embed_links=True, add_reactions=True)
+    async def manga(self, ctx: Context, *, entered_title: str) -> None:
+        """Searches for manga."""
 
-                    embed = Embed(title=f"{title}", color=ctx.author.color, rl=url)
-                    embed.description = anime["attributes"]["synopsis"][0:425] + "..."
-                    embed.add_field(name="Average Rating", value=anime["attributes"]["averageRating"])
-                    embed.add_field(name="Popularity Rank", value=anime["attributes"]["popularityRank"])
-                    embed.add_field(name="Age Rating", value=anime["attributes"]["ageRating"])
-                    embed.add_field(name="Status", value=anime["attributes"]["status"])
-                    embed.add_field(name="Aired", value=f"{anime['attributes']['startDate']}{thing}")
-                    embed.add_field(name="Episodes", value=anime["attributes"]["episodeCount"])
-                    embed.add_field(name="Type", value=anime["attributes"]["showType"])
-                    embed.set_thumbnail(url=anime["attributes"]["posterImage"]["original"])
-                    embed.set_footer(text=f"Requested by {ctx.author.name} | Powered by HotWired", icon_url=ctx.author.avatar_url_as(format="png"))
-                    try:
-                        await ctx.send(f"**{title}** - <{url}>", embed=embed)
-                    except Exception:
-                        aired = f"{anime['attributes']['startDate']}{thing}"
-                        template = textwrap.dedent(
-                            f"""
-                            ```
-                            url: {url}
-                            Title: {title}
-                            Average Rating: {anime["attributes"]["averageRating"]}
-                            Popularity Rank: {anime["attributes"]["popularityRank"]}
-                            Age Rating: {anime["attributes"]["ageRating"]}
-                            Status: {anime["attributes"]["status"]}
-                            Aired: {aired}
-                            Type: {anime['attributes']["showType"]}
-                            Powered by HotWired
-                            ```
-                            """
-                        )
-                        await ctx.send(template)
+        try:
+            cmd = "MANGA"
+            embeds, data = await self._search_anime_manga(ctx, cmd, entered_title)
 
-        @command()
-        async def manga(self, ctx: Context, *, query: str) -> None:
-            """Look up manga information."""
-            base = "https://kitsu.io/api/edge/"
+            if embeds is not None:
+                # TODO: implement pagination
+                await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
+            else:
+                await ctx.send("No mangas were found or there was an error in the process")
 
-            # Handling
-            async with ctx.typing():
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(base + "manga", params={"filter[text]": query}) as resp:
-                        resp = await resp.json()
-                        resp = resp["data"]
+        except TypeError:
+            await ctx.send("No mangas were found or there was an error in the process")
 
-                        query = utils.escape_mentions(query)
-                        query = utils.escape_markdown(query)
+    def format_name(self, first_name: str, last_name: str) -> str:  # Combines first_name and last_name and/or shows either of the two
+        if first_name and last_name:
+            return first_name + " " + last_name
+        elif first_name:
+            return first_name
+        elif last_name:
+            return last_name
+        else:
+            return "No name"
 
-                        if not resp:
-                            await ctx.send(f"No results for `{query}`.")
-                            return
+    def clean_html(self, description: str) -> str:
+        if not description:
+            return ""
+        regex = re.compile("<.*?>")
+        cleantext = re.sub(regex, "", description)
+        return cleantext
 
-                        manga = resp[0]
-                        title = f'{manga["attributes"]["canonicalTitle"]}'
-                        manga_id = manga["id"]
-                        url = f"https://kitsu.io/manga/{manga_id}"
+    def clean_spoilers(self, description: str) -> str:
+        if not description:
+            return ""
+        regex = re.compile("/<span[^>]*>.*</span>/g")
+        cleantext = re.sub(regex, "", description)
+        return cleantext
 
-                        embed = Embed(title=f"{title}", color=ctx.author.color, url=url)
-                        embed.description = manga["attributes"]["synopsis"][0:425] + "..."
+    def description_parser(self, description: str) -> str:
+        description = self.clean_spoilers(description)
+        description = self.clean_html(description)
+        description = "\n".join(description.split("\n")[:5])
+        if len(description) > 400:
+            return description[:400] + "..."
+        else:
+            return description
 
-                        if manga["attributes"]["averageRating"]:
-                            embed.add_field(name="Average Rating", value=manga["attributes"]["averageRating"])
-                        embed.add_field(name="Popularity Rank", value=manga["attributes"]["popularityRank"])
+    def list_maximum(self, items: list) -> list:
+        if len(items) > 5:
+            return items[:5] + ["+ " + str(len(items) - 5) + " more"]
+        else:
+            return items
 
-                        if manga["attributes"]["ageRating"]:
-                            embed.add_field(name="Age Rating", value=manga["attributes"]["ageRating"])
-                        embed.add_field(name="Status", value=manga["attributes"]["status"])
-                        thing = "" if not manga["attributes"]["endDate"] else f' to {manga["attributes"]["endDate"]}'
-                        embed.add_field(name="Published", value=f"{manga['attributes']['startDate']}{thing}")
+    async def _request(self, query: str, variables: str = None) -> dict:
 
-                        if manga["attributes"]["chapterCount"]:
-                            embed.add_field(name="Chapters", value=manga["attributes"]["chapterCount"])
-                        embed.add_field(name="Type", value=manga["attributes"]["mangaType"])
-                        embed.set_thumbnail(url=manga["attributes"]["posterImage"]["original"])
+        if variables is None:
+            variables = {}
 
-                        try:
-                            await ctx.send(f"**{title}** - <{url}>", embed=embed)
-                        except Exception:
-                            aired = f"{manga['attributes']['startDate']}{thing}"
-                            template = textwrap.dedent(
-                                f"""
-                                ```
-                                url: {url}
-                                Title: {title}
-                                Average Rating: {manga["attributes"]["averageRating"]}
-                                Popularity Rank: {manga["attributes"]["popularityRank"]}
-                                Age Rating: {manga["attributes"]["ageRating"]}
-                                Status: {manga["attributes"]["status"]}
-                                Aired: {aired}
-                                Type: {manga['attributes']["showType"]}
-                                Powered by HotWired
-                                ```
-                                """
-                            )
-                            await ctx.send(template)
+        request_json = {"query": query, "variables": variables}
+
+        headers = {"content-type": "application/json"}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.url, data=json.dumps(request_json), headers=headers) as response:
+                return await response.json()
+
+    async def _search_user(self, ctx: Context, entered_title: str) -> t.Union[tuple, None]:
+
+        variables = {"search": entered_title, "page": 1}
+
+        data = (await self._request(constants.SEARCH_USER_QUERY, variables))["data"]["Page"]["users"]
+
+        if data is not None and len(data) > 0:
+
+            embeds = []
+
+            for user in data:
+                link = f"https://anilist.co/user/{user['id']}"
+                title = f"[{user['name']}]({link})"
+                title = user["name"]
+
+                embed = Embed(
+                    title=title,
+                    color=Color.green(),
+                    description=self.description_parser(user["about"])
+                )
+                embed.url = link
+                embed.set_thumbnail(url=user["avatar"]["large"])
+                embed.add_field(name="Watched time", value=datetime.timedelta(minutes=int(user["stats"]["watchedTime"])))
+                embed.add_field(name="Chapters read", value=user["stats"].get("chaptersRead", "N/A"))
+
+                for category in "anime", "manga", "characters":
+                    fav = []
+                    for node in user["favourites"][category]["nodes"]:
+                        url_path = category
+                        if category == "characters":
+                            name = node["name"]
+                            title = self.format_name(name["first"], name["last"])
+                            url_path = "character"
+                        else:
+                            title = node["title"]["userPreferred"]
+
+                        fav.append(f'[{title}](https://anilist.co/{url_path}/{node["id"]})')
+
+                    if fav:
+                        embed.add_field(name=f"Favorite {category}", value="\n".join(self.list_maximum(fav)))
+                embed.set_footer(text="Powered by Anilist")
+                embeds.append(embed)
+
+            return embeds, data
+
+        else:
+            return None
+
+    @command()
+    async def character(self, ctx: Context, *, entered_title: str) -> None:
+        """Searches for characters."""
+
+        try:
+            embeds, data = await self._search_character(ctx, entered_title)
+
+            if embeds is not None:
+                # TODO: implement pagination
+                await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
+            else:
+                await ctx.send("No characters were found or there was an error in the process")
+
+        except TypeError:
+            await ctx.send("No characters were found or there was an error in the process")
+
+    @command()
+    @bot_has_permissions(embed_links=True, add_reactions=True)
+    async def user(self, ctx: Context, *, entered_title: str) -> None:
+        """Searches users."""
+
+        try:
+            embeds, data = await self._search_user(ctx, entered_title)
+
+            if embeds is not None:
+                # TODO: implement pagination
+                await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=30)
+            else:
+                await ctx.send("No users were found or there was an error in the process")
+
+        except TypeError:
+            await ctx.send("No users were found or there was an error in the process")
 
 
 def setup(bot: Bot) -> None:
--- cogs/utils/constants.py
+++ cogs/utils/constants.py
@@ -228,6 +228,124 @@ with open('cogs/utils/languages.yml', 'r') as file:
     default = safe_load(file)
 
 
+SEARCH_ANIME_MANGA_QUERY = """
+query ($id: Int, $page: Int, $search: String, $type: MediaType) {
+    Page (page: $page, perPage: 10) {
+        media (id: $id, search: $search, type: $type) {
+            id
+            idMal
+            description(asHtml: false)
+            title {
+                english
+                romaji
+            }
+            coverImage {
+                    medium
+            }
+            bannerImage
+            averageScore
+            meanScore
+            status
+            episodes
+            chapters
+            externalLinks {
+                url
+                site
+            }
+            nextAiringEpisode {
+                timeUntilAiring
+            }
+        }
+    }
+}
+"""
+
+SEARCH_CHARACTER_QUERY = """
+query ($id: Int, $page: Int, $search: String) {
+  Page(page: $page, perPage: 10) {
+    characters(id: $id, search: $search) {
+      id
+      description (asHtml: true),
+      name {
+        first
+        last
+        native
+      }
+      image {
+        large
+      }
+      media {
+        nodes {
+          id
+          type
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+SEARCH_USER_QUERY = """
+query ($id: Int, $page: Int, $search: String) {
+    Page (page: $page, perPage: 10) {
+        users (id: $id, search: $search) {
+            id
+            name
+            siteUrl
+            avatar {
+                    large
+            }
+            about (asHtml: true),
+            stats {
+                watchedTime
+                chaptersRead
+            }
+            favourites {
+            manga {
+              nodes {
+                id
+                title {
+                  romaji
+                  english
+                  native
+                  userPreferred
+                }
+              }
+            }
+            characters {
+              nodes {
+                id
+                name {
+                  first
+                  last
+                  native
+                }
+              }
+            }
+            anime {
+              nodes {
+                id
+                title {
+                  romaji
+                  english
+                  native
+                  userPreferred
+                }
+              }
+            }
+            }
+        }
+    }
+}
+"""
+
+
 class Infraction(Enum):
     warning = Color.gold()
     kick = Color.gold()
