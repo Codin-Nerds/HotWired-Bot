2a34eaf8dfbdb067076ae25ecb35c618c07445d7
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-13 14:52:55 +0200

    Cleanup `errors.py`

--- bot/utils/errors.py
+++ bot/utils/errors.py
@@ -2,12 +2,6 @@ from discord.ext import commands
 from discord import NotFound
 
 
-class PostException(Exception):
-    """Raised when there's a error in the post() wrapper."""
-
-    pass
-
-
 class ServiceError(commands.CommandInvokeError):
     """Raised whenever a request to a service and returns a failure of some sort."""
 
@@ -20,18 +14,7 @@ class NSFWException(commands.CheckFailure):
     pass
 
 
-class ConfigError(Exception):
-    """Raised when there is an error in the configuration file."""
-
-    pass
-
-
-class WeatherException(Exception):
-    """Raised when there is an error attempting to access weather data."""
-
-    pass
-
-
 class MemberNotFound(NotFound):
     """Raised when search for Member has failed and no member was found."""
+
     pass
