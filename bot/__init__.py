593add1feb6d50e72be6e50c1dcc847a35321abb
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-14 17:28:37 +0200

    Add basic logging settings

---
+++ bot/__init__.py
@@ -0,0 +1,17 @@
+
+import sys
+
+from loguru import logger
+
+from bot.config import DEV_MODE
+
+log_file = "logs/bot.log"
+log_format = "<green>{time:YYYY-MM-DD hh:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan> | <level>{message}</level>"
+log_level = "DEBUG" if DEV_MODE else "INFO"
+
+logger.configure(
+    handlers=[
+        dict(sink=sys.stdout, format=log_format, level=log_level),
+        dict(sink=log_file, format=log_format, level=log_level, rotation="500 MB")
+    ]
+)
