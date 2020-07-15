ff63247fc6ea471399eb87c7f354f761e224bf69
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-15 14:51:33 +0200

    Adjust logging format
    - Use `<18` on name to make sure all logged messages are tabulated under
      each other, preventing bad looking log messages

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,7 +6,7 @@ from loguru import logger
 from bot.config import DEV_MODE
 
 log_file = "logs/bot.log"
-log_format = "<green>{time:YYYY-MM-DD hh:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan> | <level>{message}</level>"
+log_format = "<green>{time:YYYY-MM-DD hh:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name: <18}</cyan> | <level>{message}</level>"
 log_level = "DEBUG" if DEV_MODE else "INFO"
 
 logger.configure(
