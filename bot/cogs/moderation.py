a25b2076449f9dfcb2b737712200df42ba604f94
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-17 12:39:20 +0200

    Add send embed functions

--- bot/cogs/moderation.py
+++ bot/cogs/moderation.py
@@ -1,11 +1,21 @@
+import textwrap
 import typing as t
+from datetime import datetime
 
-from discord.ext.commands import (Cog, Context, NoPrivateMessage,
-                                  RoleConverter, command, has_permissions)
+from discord import Color, Embed, Member, User
+from discord.ext.commands import (
+    Cog,
+    Context,
+    NoPrivateMessage,
+    RoleConverter,
+    command,
+    has_permissions,
+)
 
 from bot.core.bot import Bot
 from bot.core.converters import Duration, ProcessedMember, ProcessedUser
 from bot.core.decorators import follow_roles
+from discord.errors import Forbidden
 
 
 class Moderation(Cog):
@@ -15,21 +25,21 @@ class Moderation(Cog):
     # region: Commands
 
     @command()
-    @follow_roles("member")
+    @follow_roles()
     @has_permissions(kick_members=True)
     async def kick(self, ctx: Context, member: ProcessedMember, *, reason: str = "No specific reason.") -> None:
         """Kick user from the server for given reason."""
-        await self._apply_kick()
+        await self._apply_kick(ctx, member, reason)
 
     @command()
-    @follow_roles("user")
+    @follow_roles()
     @has_permissions(administrator=True)
-    async def ban(self, ctx: Context, user: ProcessedUser, *, reason: str = "No specific reason,") -> None:
+    async def ban(self, ctx: Context, user: ProcessedUser, *, reason: str = "No specific reason.") -> None:
         """"Permanently ban user from the server for given reason."""
-        await self._apply_ban()
+        await self._apply_ban(ctx, user, reason)
 
     @command()
-    @follow_roles("user")
+    @follow_roles()
     @has_permissions(ban_members=True)
     async def tempban(self, ctx: Context, user: ProcessedUser, duration: Duration, *, reason: str = "No specific reason.") -> None:
         """Temporarely ban user from the server for given reason."""
@@ -39,10 +49,10 @@ class Moderation(Cog):
     @has_permissions(ban_members=True)
     async def unban(self, ctx: Context, user: ProcessedUser) -> None:
         """Revoke users ban on the server."""
-        await self._apply_unban()
+        await self._apply_unban(ctx, user)
 
     @command()
-    @follow_roles("member")
+    @follow_roles()
     @has_permissions(manage_roles=True)
     async def promote(self, ctx: Context, member: ProcessedMember, role: RoleConverter, *, reason: str = "No specific reason.") -> None:
         """Promote user to any given role (less than yours)."""
@@ -63,14 +73,20 @@ class Moderation(Cog):
     # endregion
     # region: Strike Execution (private) functions
 
-    async def _apply_kick(self) -> None:
-        pass
+    async def _apply_kick(self, ctx: Context, member: Member, reason: str) -> None:
+        dm_sent = await self._send_DM(member, ctx, "Kicked", reason)
+        await self._send_server_embed(member, ctx, "Kicked", reason)
+        await ctx.send(dm_sent)
 
-    async def _apply_ban(self) -> None:
-        pass
+    async def _apply_ban(self, ctx: Context, user: User, reason: str) -> None:
+        dm_sent = await self._send_DM(user, ctx, "Banned", reason)
+        await self._send_server_embed(user, ctx, "Banned", reason)
+        await ctx.send(dm_sent)
 
-    async def _apply_unban(self) -> None:
-        pass
+    async def _apply_unban(self, ctx: Context, user: User) -> None:
+        dm_sent = await self._send_DM(user, ctx, "Unbanned", "", color=Color.green())
+        await self._send_server_embed(user, ctx, "Unbanned", "", color=Color.green())
+        await ctx.send(dm_sent)
 
     async def _apply_promotion(self) -> None:
         pass
@@ -80,6 +96,43 @@ class Moderation(Cog):
 
     async def _apply_cleanup(self) -> None:
         pass
+
+    async def _send_DM(self, user: t.Union[Member, User], ctx: Context, action: str, reason: str, color: Color = Color.red()) -> bool:
+        reason_string = f"{reason}\n" if reason else '\r'
+        embed = Embed(
+            title=f"You were {action}",
+            description=textwrap.dedent(
+                f"""
+                {reason_string}
+                *Server: {ctx.guild.name}*
+                """
+            ),
+            color=color,
+            timestamp=datetime.utcnow(),
+        )
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        try:
+            await user.send(embed=embed)
+            return True
+        except Forbidden:
+            return False
+
+    async def _send_server_embed(self, user: t.Union[Member, User], ctx: Context, action: str, reason: str, color: Color = Color.red()) -> None:
+        reason_string = f"**Reason**: {reason}\n" if reason else '\r'
+        embed = Embed(
+            title=f"User {action}",
+            description=textwrap.dedent(
+                f"""
+                {reason_string}
+                **User**: {user.mention} (`{user.id}`)
+                **Moderator**: {ctx.author.mention} (`{ctx.author.id}`)
+                """
+            ),
+            color=color,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_thumbnail(url=user.avatar_url_as(format="png", size=256))
+        await ctx.send(embed=embed)
 
     # endregion
 
