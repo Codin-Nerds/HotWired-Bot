d7944558466e4cb016758f41d4c0869666071a9f
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-21 01:38:18 +0200

    Rework TicTacToe
    - Use a custom class for the game
    - Use default dict of default dicts which stores `guild` as first key
      and `player` as second key to keep track of the active games. This
      dict is used to store instances of the new Game class
    - Use actual 2D list instead of using a flat version
    - Implement a better `check_win` function
    - Use embed to show the game field and redraw it on each new turn
    - Define reactions in a single list (no need to repeat ourselves)
    - Temporarely remove the old AI functions, they will be implemented
      later

--- bot/cogs/games/tic_tac_toe.py
+++ bot/cogs/games/tic_tac_toe.py
@@ -1,9 +1,161 @@
 import typing as t
+from collections import defaultdict
 
 from bot.core.bot import Bot
+from bot.core.decorators import ProcessedMember
 
-from discord import TextChannel, Message, Reaction, User
+from discord import Color, Embed, Member, Reaction, User
 from discord.ext.commands import Cog, Context, command
+
+
+class Game:
+    """Internal Tic Tac Toe logic."""
+    reactions = [
+        "\u2196",  # top left
+        "\u2B06",  # top
+        "\u2197",  # top right
+        "\u2B05",  # left
+        "\u23FA",  # middle
+        "\u27A1",  # right
+        "\u2199",  # bottom left
+        "\u2B07",  # bottom
+        "\u2198",  # bottom right
+    ]
+
+    def __init__(self, ctx: Context, opponent: t.Optional[Member]) -> None:
+        self.ctx = ctx
+        self.author = ctx.author
+        self.player = "x"
+        self.opponent = opponent
+        self.cpu_opponent = opponent is None
+        self.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+        ]
+
+    def next_player(self) -> None:
+        """Switch player."""
+        if self.player == "x":
+            self.player = "o"
+        else:
+            self.player = "x"
+
+    async def send_board(self) -> None:
+        """Send embed with the board,"""
+        embed = Embed(
+            title="Tic Tac Toe",
+            description=f"```{self.str_board}```",
+            color=Color.blurple()
+        )
+        if not hasattr(self, "msg"):
+            self.msg = await self.ctx.send(embed=embed)
+            await self.add_buttons()
+        else:
+            await self.msg.edit(embed=embed)
+
+    async def add_buttons(self) -> None:
+        """Add reaction buttons to the message."""
+        for reaction in self.reactions:
+            await self.msg.add_reaction(reaction)
+
+    def apply_emoji_move(self, reaction_emoji: str, xo: t.Literal["x", "o"]) -> bool:
+        """Apply move based on an emoji."""
+        try:
+            move_id = self.reactions.index(reaction_emoji)
+        except ValueError:
+            # Bad emoji passed
+            return False
+
+        row = move_id // 3
+        col = move_id % 3
+
+        if not self.board[row][col]:
+            self.board[row][col] = xo
+            return True
+        return False
+
+    @property
+    def str_board(self) -> str:
+        """Convert the board 2D array to string"""
+        result = ""
+
+        for row in self.board:
+            result += "|".join(f"{val: ^{2}}" for val in row)
+            result += f"\n{'-' * 11}\n"
+
+        return "\n".join(result.splitlines()[:-1])
+
+    def check_win(self) -> t.Tuple[bool, t.Literal["x", "o", None]]:
+        """Check if the board is in win status, if it is return (True, winner)"""
+
+        # Check rows and columns for X streaks
+        row_stat = {"x": [0, 0, 0], "o": [0, 0, 0]}
+        col_stat = {"x": [0, 0, 0], "o": [0, 0, 0]}
+
+        for row_id, row in enumerate(self.board):
+            for col_id, col in enumerate(row):
+                if col:
+                    row_stat[col][row_id] += 1
+                    col_stat[col][col_id] += 1
+
+        if 3 in row_stat["x"] or 3 in col_stat["x"]:
+            return (True, "x")
+        if 3 in row_stat["o"] or 3 in col_stat["o"]:
+            return (True, "o")
+
+        # Check diagonals
+        diag_1 = [row[col] for col, row in enumerate(self.board)]
+        diag_2 = [row[(col + 1) * -1] for col, row in enumerate(self.board)]
+
+        for xo in ["x", "o"]:
+            if all(field == xo for field in diag_1):
+                return (True, xo)
+            if all(field == xo for field in diag_2):
+                return (True, xo)
+
+        return (False, None)
+
+    def check_draw(self) -> bool:
+        """Check if current board is in draw."""
+        return all(xo != "" for row in self.board for xo in row)
+
+    async def turn(self, member: Member, reaction: Reaction) -> bool:
+        """A single turn of the game, returns ended status of game (True = ended)."""
+        if not any([
+            (self.opponent == member and self.player == "o"),
+            (self.author == member and self.player == "x"),
+        ]):
+            await self.ctx.send(f"Hey {member.mention}, it's not your turn!")
+            await reaction.remove(member)
+            return False
+
+        if not self.apply_emoji_move(str(reaction.emoji), self.player):
+            await self.ctx.send(f"Hey {member.mention}, that tile is occupied!")
+            await reaction.remove(member)
+            return False
+
+        await self.send_board()
+        win, xo = self.check_win()
+        if win:
+            if xo == "x":
+                winner = self.author
+            else:
+                winner = self.opponent
+
+            if winner:
+                await self.ctx.send(f":tada: Congratulations {winner.mention}, you won!")
+            else:
+                await self.ctx.send(f"You lost {self.author.mention}, better luck next time!")
+
+            return True
+
+        if self.check_draw():
+            await self.ctx.send("Game result: draw")
+            return True
+
+        self.next_player()
+        return False
 
 
 class TicTacToe(Cog):
@@ -11,227 +163,47 @@ class TicTacToe(Cog):
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
-        self.ttt_games = {}
+        self.games = defaultdict(lambda: defaultdict(lambda: None))
 
     @command(aliases=["ttt", "tictactoe"])
-    async def tic_tac_toe(self, ctx: Context, move: str = "") -> None:
+    async def tic_tac_toe(self, ctx: Context, opponent: t.Optional[ProcessedMember] = None) -> None:
         """Play a game of Tic-Tac-Toe."""
-        await self.ttt_new(ctx.author, ctx.channel)
+        if self.games[ctx.guild][ctx.author]:
+            await ctx.send(f"Sorry {ctx.author.mention}, you already have an active tic tac toe game on this server.")
+            return
+        if self.games[ctx.guild][ctx.author]:
+            await ctx.send(
+                f"Sorry, {ctx.author.mention}, {opponent.mention} already has an active tic tac toe game on this server, let him finish first."
+            )
+            return
 
-    async def ttt_new(self, user: User, channel: TextChannel) -> None:
-        self.ttt_games[user.id] = [" "] * 9
-        response = self.ttt_make_board(user)
-        response += "Your move:"
-        msg = await self.bot.send_message(channel, response)
-        await self.makeButtons(msg)
+        game = Game(ctx, opponent)
+        self.games[ctx.guild][ctx.author] = game
+        if opponent:
+            self.games[ctx.guild][opponent] = game
+        await game.send_board()
 
-    async def ttt_move(self, user: User, message: Message, move: int) -> None:
-        print(f"ttt_move:{user.id}")
-        uid = user.id
-        if uid not in self.ttt_games:
-            print("New game")
-            return await self.ttt_new(user, message.channel)
-
-        # Check spot is empty
-        if self.ttt_games[uid][move] == " ":
-            self.ttt_games[uid][move] = "x"
-            print(f"Moved to {move}")
+    @command(aliases=["tttstop", "tictactoestop"])
+    async def tic_tac_toe_stop(self, ctx: Context) -> None:
+        """Stop playing tic tac toe."""
+        if self.games[ctx.guild][ctx.author]:
+            del self.games[ctx.guild][ctx.author]
+            await ctx.send("Game stopped.")
         else:
-            print(f"Invalid move: {move}")
-            return None
+            await ctx.send(f"Sorry {ctx.author.mention}, you don't have an active game of tic tac toe on this server.")
 
-        # Check winner
-        check = self.tttDoChecks(self.ttt_games[uid])
-        if check is not None:
-            msgAppend = "It's a draw!" if check == "draw" else f"{check[-1]} wins!"
-            print(msgAppend)
-            await self.bot.edit_message(message, new_content=f"{self.ttt_make_board(user)}{msgAppend}")
-            return None
-        print("Check passed")
-
-        # AI move
-        mv = self.tttAIThink(self.tttMatrix(self.ttt_games[uid]))
-        self.ttt_games[uid][self.tttCoordsToIndex(mv)] = "o"
-        print("AI moved")
-
-        # Update board
-        await self.bot.edit_message(message, new_content=self.ttt_make_board(user))
-        print("Board updated")
-
-        # Check winner again
-        check = self.tttDoChecks(self.ttt_games[uid])
-        if check is not None:
-            msgAppend = "It's a draw!" if check == "draw" else f"{check[-1]} wins!"
-            print(msgAppend)
-            await self.bot.edit_message(message, new_content="{self.ttt_make_board(user)}{msgAppend}")
-        print("Check passed")
-
-    def ttt_make_board(self, author: User) -> str:
-        return f"{author.mention}\n{self.tttTable(self.ttt_games[author.id])}\n"
-
-    async def makeButtons(self, msg: Message) -> None:
-        await self.bot.add_reaction(msg, u"\u2196")  # 0 tl
-        await self.bot.add_reaction(msg, u"\u2B06")  # 1 t
-        await self.bot.add_reaction(msg, u"\u2197")  # 2 tr
-        await self.bot.add_reaction(msg, u"\u2B05")  # 3 l
-        await self.bot.add_reaction(msg, u"\u23FA")  # 4 mid
-        await self.bot.add_reaction(msg, u"\u27A1")  # 5 r
-        await self.bot.add_reaction(msg, u"\u2199")  # 6 bl
-        await self.bot.add_reaction(msg, u"\u2B07")  # 7 b
-        await self.bot.add_reaction(msg, u"\u2198")  # 8 br
-
+    @Cog.listener()
     async def on_reaction_add(self, reaction: Reaction, user: User) -> None:
-        if reaction.message.author.id == self.bot.user.id and not user.id == self.bot.user.id:
-            move = self.decodeMove(str(reaction.emoji))
-            if move is not None:
-                await self.ttt_move(user, reaction.message, move)
+        # TODO: Use a custom function for converting User to Member
+        guild = reaction.message.guild
+        member = guild.get_member(user.id)
+        game = self.games[guild][member]
 
-    def decodeMove(self, emoji: str) -> None:
-        emoji_dict = {
-            u"\u2196": 0,
-            u"\u2B06": 1,
-            u"\u2197": 2,
-            u"\u2B05": 3,
-            u"\u23FA": 4,
-            u"\u27A1": 5,
-            u"\u2199": 6,
-            u"\u2B07": 7,
-            u"\u2198": 8
-        }
-        return emoji_dict[emoji] if emoji in emoji_dict else None
-
-    # Utility Functions
-    def tttTable(self, xo) -> str:
-        return (("%s%s%s\n" * 3) % tuple(xo)).replace("o", ":o2:").replace("x", ":regional_indicator_x:").replace(" ", ":white_large_square:")
-
-    def tttMatrix(self, b: list) -> list:
-        return [
-            [b[0], b[1], b[2]],
-            [b[3], b[4], b[5]],
-            [b[6], b[7], b[8]]
-        ]
-
-    def tttCoordsToIndex(self, coords: tuple) -> str:
-        board_map = {
-            (0, 0): 0,
-            (0, 1): 1,
-            (0, 2): 2,
-            (1, 0): 3,
-            (1, 1): 4,
-            (1, 2): 5,
-            (2, 0): 6,
-            (2, 1): 7,
-            (2, 2): 8
-        }
-        return board_map[coords]
-
-    def tttDoChecks(self, b: str) -> t.Optional[str]:
-        m = self.tttMatrix(b)
-        if self.tttCheckWin(m, "x"):
-            return "win X"
-        if self.tttCheckWin(m, "o"):
-            return "win O"
-        if self.tttCheckDraw(b):
-            return "draw"
-        return None
-
-    def tttFindStreaks(self, m: list, xo: str) -> tuple:
-        row = [0, 0, 0]
-        col = [0, 0, 0]
-        dia = [0, 0]
-
-        # Check rows and columns for X streaks
-        for y in range(3):
-            for x in range(3):
-                if m[y][x] == xo:
-                    row[y] += 1
-                    col[x] += 1
-
-        # Check diagonals
-        if m[0][0] == xo:
-            dia[0] += 1
-        if m[1][1] == xo:
-            dia[0] += 1
-            dia[1] += 1
-        if m[2][2] == xo:
-            dia[0] += 1
-        if m[2][0] == xo:
-            dia[1] += 1
-        if m[0][2] == xo:
-            dia[1] += 1
-
-        return (row, col, dia)
-
-    def tttFindEmpty(self, matrix: list, rcd: str, n: int) -> int:
-        # Rows
-        if rcd == "r":
-            for x in range(3):
-                if matrix[n][x] == " ":
-                    return x
-        # Columns
-        if rcd == "c":
-            for x in range(3):
-                if matrix[x][n] == " ":
-                    return x
-        # Diagonals
-        if rcd == "d":
-            if n == 0:
-                for x in range(3):
-                    if matrix[x][x] == " ":
-                        return x
-            else:
-                for x in range(3):
-                    if matrix[x][2 - x] == " ":
-                        return x
-
-        return False
-
-    def tttCheckWin(self, m: list, xo: str) -> bool:
-        row, col, dia = self.tttFindStreaks(m, xo)
-        dia.append(0)
-
-        for i in range(3):
-            if row[i] == 3 or col[i] == 3 or dia[i] == 3:
-                return True
-
-        return False
-
-    def tttCheckDraw(self, board: list) -> bool:
-        return " " not in board
-
-    def tttAIThink(self, m: list) -> t.Union[tuple, bool]:
-        rx, cx, dx = self.tttFindStreaks(m, "x")
-        ro, co, do = self.tttFindStreaks(m, "o")
-
-        mv = self.tttAIMove(2, m, ro, co, do)
-        if mv is not False:
-            return mv
-        mv = self.tttAIMove(2, m, rx, cx, dx)
-        if mv is not False:
-            return mv
-        mv = self.tttAIMove(1, m, ro, co, do)
-        if mv is not False:
-            return mv
-        return self.tttAIMove(1, m, rx, cx, dx)
-
-    def tttAIMove(self, n: str, m: list, row: list, col: list, dia: list) -> t.Union[tuple, bool]:
-        for r in range(3):
-            if row[r] == n:
-                x = self.tttFindEmpty(m, "r", r)
-                if x is not False:
-                    return (r, x)
-            if col[r] == n:
-                y = self.tttFindEmpty(m, "c", r)
-                if y is not False:
-                    return (y, r)
-
-        if dia[0] == n:
-            y = self.tttFindEmpty(m, "d", 0)
-            if y is not False:
-                return (y, y)
-        if dia[1] == n:
-            y = self.tttFindEmpty(m, "d", 1)
-            if y is not False:
-                return (y, 2 - y)
-
-        return False
+        if game:
+            ended = await game.turn(member, reaction)
+            if ended:
+                if game.opponent == member:
+                    del self.games[guild][game.author]
+                elif game.author == member and not game.cpu_opponent:
+                    del self.games[guild][game.opponent]
+                del self.games[guild][member]
