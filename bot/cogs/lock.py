430ec357b7c3b8361c34a8950020176ace4a7b6a
janaSunrise <warriordefenderz@gmail.com>
2020-07-23 23:41:27 +0530

    Implement banlock, and kiclock mode toggle

--- bot/cogs/lock.py
+++ bot/cogs/lock.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Cog, Context
+from discord.ext.commands import Cog, Context, command
 
 from bot.core.bot import Bot
 from bot.utils.sqlitefunc import enable_banlock, enable_kicklock, disable_banlock, disable_kicklock
@@ -8,25 +8,29 @@ class Lock(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    def banlock(self, ctx: Context) -> None:
+    @command()
+    async def banlock(self, ctx: Context) -> None:
         """Enable locking to Ban all the new members joining the guild."""
         enable_banlock(ctx.guild.id)
 
         await ctx.send("Banlock successfully Enabled")
 
-    def banunlock(self, ctx: Context) -> None:
+    @command()
+    async def banunlock(self, ctx: Context) -> None:
         """Disable locking to Ban all the new members joining the guild."""
         disable_banlock(ctx.guild.id)
 
         await ctx.send("Banlock successfully Disabled")
 
-    def kicklock(self, ctx: Context) -> None:
+    @command()
+    async def kicklock(self, ctx: Context) -> None:
         """Enable locking to Kick all the new members joining the guild."""
         enable_kicklock(ctx.guild.id)
 
         await ctx.send("Kicklock successfully Enabled")
 
-    def kickunlock(self, ctx: Context) -> None:
+    @command()
+    async def kickunlock(self, ctx: Context) -> None:
         """Disable locking to Kick all the new members joining the guild."""
         disable_kicklock(ctx.guild.id)
 
