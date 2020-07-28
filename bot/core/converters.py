376dfdaabc5715ae838dae44a22724d76458a6ba
ItsDrike <itsdrikeofficial@gmail.com>
2020-07-28 17:26:22 +0200

    Adjust `ProcessedMember and `ProcessedUser` converters

--- bot/core/converters.py
+++ bot/core/converters.py
@@ -3,11 +3,27 @@ import typing as t
 from ast import literal_eval
 from contextlib import suppress
 
-from discord import Guild, Member, NotFound, User
-from discord.ext.commands import BadArgument, Context, Converter, UserConverter
+from discord import Member, NotFound, User
+from discord.ext.commands import (
+    BadArgument, Context, Converter,
+    MemberConverter, UserConverter
+)
 
-from bot.utils.errors import MemberNotFound
+from bot.utils.errors import MemberNotFound, UserNotFound
 from dateutil.relativedelta import relativedelta
+
+
+def _obtain_user_id(argument: str) -> t.Optional[int]:
+    """Get user ID from mention or directly from string."""
+    mention_match = re.match(r'<@!?([0-9]+)>$', argument)
+    id_match = re.match(r'([0-9]{15,21})$', argument)
+
+    if mention_match is None and id_match is None:
+        return None
+    elif mention_match is not None:
+        return int(mention_match.group(1))
+    elif id_match is not None:
+        return int(id_match.group(1))
 
 
 class ActionReason(Converter):
@@ -103,39 +119,56 @@ class TimeDelta(Converter):
 
 class ProcessedUser(UserConverter):
     """
-    Try to convert any accepted string into `Member` or `User`.
-
-    When possible try to convert user into `Member` but if not, use `User` instead.
+    Try to convert any accepted string into `User`
+    Lookup Strategy:
+    [Default UserConverter strategy]
+    1. Lookup by ID.
+    2. Lookup by mention.
+    3. Lookup by name#discrim
+    4. Lookup by name
+    [Added functionality]
+    5. Lookup by API
     """
 
-    async def get_member(guild: Guild, user: User) -> Member:
-        member = guild.get_member(user.id)
-        if not member:
-            try:
-                member = await guild.fetch_member(user.id)
-            except NotFound:
-                raise MemberNotFound(f"No member with ID: {user.id} on guild {guild.id}")
-        return member
+    async def convert(self, ctx: Context, argument: str) -> User:
+        # Follow general MemberConverter lookup strategy
+        with suppress(BadArgument):
+            return await super().convert(ctx, argument)
+
+        # Try to look user up using API
+        ID = _obtain_user_id(argument)
+        if ID is None:
+            raise UserNotFound(f"No user found from `{argument}`")
+        try:
+            return await ctx.bot.fetch_user(ID)
+        except NotFound:
+            raise UserNotFound(f"No user with ID: {ID} found")
+
+
+class ProcessedMember(MemberConverter):
+    """
+    Try to convert any accepted string into `Member`
+    Lookup Strategy:
+    [Default MemberConverter strategy]
+    1. Lookup by ID.
+    2. Lookup by mention.
+    3. Lookup by name#discrim
+    4. Lookup by name
+    5. Lookup by nickname
+    [Added functionality]
+    6. Lookup by API
+    """
 
     async def convert(self, ctx: Context, argument: str) -> Member:
-        """Convert the `argument` into `Member` or `User`."""
+        # Follow general MemberConverter lookup strategy
         with suppress(BadArgument):
-            # Try to use UserConverter first
-            user = await super().convert(ctx, argument)
-            try:
-                return await self.get_member(ctx.guild, user)
-            except MemberNotFound:
-                return user
+            return await super().convert(ctx, argument)
 
-        # If UserConverter failed, try to fetch user as ID
+        # Try to look user up using API
+        ID = _obtain_user_id(argument)
+        if ID is None:
+            raise MemberNotFound(f"No member found on guild {ctx.guild.id} from `{argument}`")
         try:
-            user = await ctx.bot.fetch_user(int(argument))
-            try:
-                return await self.get_member(ctx.guild, user)
-            except MemberNotFound:
-                return user
-        except ValueError:
-            raise BadArgument(f"{argument} is not a valid user or user ID")
-
-
-ProcessedMember = t.Union[Member, ProcessedUser]
+            return await ctx.guild.fetch_member(ID)
+        except NotFound:
+            raise MemberNotFound(f"No member with ID: {ID} found on guild {ctx.guild.id}")
