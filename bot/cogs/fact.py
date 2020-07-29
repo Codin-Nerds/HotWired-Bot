1daa73853b63d34ec407cafb2f39fa55cd213b81
DudeDaCode <68649648+DudeDaCode@users.noreply.github.com>
2020-07-29 13:18:37 +0530

    Update

--- bot/cogs/fact.py
+++ bot/cogs/fact.py
@@ -2,7 +2,8 @@ import os
 import Discord 
 import randfacts
 
-@command(aliases=["fact"])
-    async def serverinfo(self, ctx: Context) -> None:
+@command
+    async def randomfact(self, ctx: Context) -> None:
         """Get a randaom fact"""
-        await ctx.channel.send(randfacts.GetFact)
+        fact = randfacts.GetFact
+        await ctx.channel.send(fact]
