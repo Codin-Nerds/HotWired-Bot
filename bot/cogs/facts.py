5f07899fbcd8226692bcd4f12993d1ff51b83659
DudeDaCode <68649648+DudeDaCode@users.noreply.github.com>
2020-07-29 13:55:20 +0530

    Update

--- bot/cogs/facts.py
+++ bot/cogs/facts.py
@@ -11,7 +11,7 @@ class Facts(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         
-        @command()
+    @command()
     async def randomfact(self, ctx: Context) -> None:
         """Get a randaom fact"""
         randomfact = randfacts.GetFact
