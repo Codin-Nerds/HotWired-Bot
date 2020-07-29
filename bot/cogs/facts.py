f094801a2bbce485e96fed8bd4b0036eaf0be7e6
DudeDaCode <Sayakdattagupta@gmail.com>
2020-07-29 14:59:08 +0530

    Fixed Flake8 Errors

--- bot/cogs/facts.py
+++ bot/cogs/facts.py
@@ -1,16 +1,17 @@
-import os 
 from discord import Embed, Color
 import randfacts
 
 from discord.ext.commands import Cog, Context, command
 
-from bot import config
 from bot.core.bot import Bot
 
+
 class Facts(Cog):
+    """A Class For A Random Fact"""
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
-        
+
     @command()
     async def randomfact(self, ctx: Context) -> None:
         """Get A Random fact"""
@@ -20,11 +21,19 @@ class Facts(Cog):
             description=randomfact["text"],
             color=Color.blurple()
         ))
-            
+
+# E
+# E
+# E
+# E
+# E
+# E
+# E
+# E
+# E
+# E
+# E
+
+
 def setup(bot: Bot) -> None:
     bot.add_cog(Facts(bot))
-
-
-
-
-       
