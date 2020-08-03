9189c3e22d106656cf676bea39a015f249327f3c
ItsDrike <itsdrikeofficial@gmail.com>
2020-08-03 19:23:27 +0200

    Move fun and comics to games

--- bot/cogs/games/fun.py
+++ bot/cogs/games/fun.py
@@ -471,7 +471,3 @@ class Fun(Cog):
 
 # TODO: kiss, hug, pat => commands to be added cuddle hug insult kiss lick nom pat poke slap stare highfive bite
 #  greet punch handholding tickle kill hold pats wave boop
-
-
-def setup(bot: Bot) -> None:
-    bot.add_cog(Fun(bot))
