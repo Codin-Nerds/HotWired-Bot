cc8b0d4952d7e9dac8db2108eab9eeb71bd116a4
Warrior Defend <56873669+janaSunrise@users.noreply.github.com>
2020-08-05 22:42:31 +0530

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -425,9 +425,5 @@ class Fun(Cog):
         await ctx.send(embed=embed)
 
 
-# TODO: kiss, hug, pat => commands to be added cuddle hug insult kiss lick nom pat poke slap stare highfive bite
-#  greet punch handholding tickle kill hold pats wave boop
-
-
 def setup(bot: Bot) -> None:
     bot.add_cog(Fun(bot))
