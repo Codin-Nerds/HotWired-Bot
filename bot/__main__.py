9a8fbf4495a2bc82aa5a714b3c1aeb1aac8c9b98
Warrior Defend <56873669+janaSunrise@users.noreply.github.com>
2020-08-07 13:37:49 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,24 +13,35 @@ extensions = [
     "bot.cogs.codesandbox",
     "bot.cogs.coding",
     "bot.cogs.commands",
-    "bot.cogs.comics",
     "bot.cogs.common",
     "bot.cogs.conversion",
+
+    "bot.cogs.disjobs",
     "bot.cogs.documentation",
+
     "bot.cogs.embeds",
     "bot.cogs.emotes",
     "bot.cogs.events",
+
     "bot.cogs.games",
     "bot.cogs.github",
+
     "bot.cogs.help",
+
     "bot.cogs.moderation",
+    "bot.cogs.music",
+
     "bot.cogs.nasa",
+    "bot.cogs.nsfw",
+
     "bot.cogs.reddit",
+
     "bot.cogs.search",
     "bot.cogs.security",
     "bot.cogs.study",
     "bot.cogs.sudo",
     "bot.cogs.support",
+
     "bot.cogs.tools",
     "bot.cogs.translate",
 ]
