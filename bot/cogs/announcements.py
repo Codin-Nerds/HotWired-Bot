e13af5728d217911bc8867f48df1c4f994bb2216
janaSunrise <warriordefenderz@gmail.com>
2020-08-27 19:05:43 +0530

    add announcements

---
+++ bot/cogs/announcements.py
@@ -0,0 +1,74 @@
+from discord import (
+    Color,
+    Embed,
+    Role
+)
+from discord.ext.commands import (
+    Cog,
+    Context,
+    command,
+    check,
+)
+
+
+class Common(Cog):
+    """Common commands."""
+
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @command()
+    def subscribe(self, ctx: Context) -> None:
+        async with self.bot.pool.acquire() as database:
+            row = await database.fetchrow(
+                "SELECT * FROM public.subscribe WHERE guild_id=$1",
+                ctx.guild.id
+            )
+
+        if not row:
+            await ctx.send("ERROR! The Announcement role hasn't been configured for this server!")
+
+        role = discord.utils.find(lambda r: r.id == row["role_id"], ctx.guild.roles)
+        if role in user.roles:
+            await ctx.send("You're already subscribed!")
+        else:
+            user.add_roles(role)
+            await ctx.send("You're Finally Subscribed!")
+
+    @command()
+    def unsubscribe(self, ctx: Context) -> None:
+        async with self.bot.pool.acquire() as database:
+            row = await database.fetchrow(
+                "SELECT * FROM public.subscribe WHERE guild_id=$1",
+                ctx.guild.id
+            )
+
+        if not row:
+            await ctx.send("ERROR! The Announcement role hasn't been configured for this server!")
+
+        role = discord.utils.find(lambda r: r.id == row["role_id"], ctx.guild.roles)
+        if role not in user.roles:
+            await ctx.send("You're already unsubscribed!")
+        else:
+            user.remove_roles(role)
+            await ctx.send("Oh No! You unsubscribed!")
+
+    @command()
+    @check(manage_roles=True)
+    def announcement_role(self, ctx: Context, role: Role) -> None:
+        if isinstance(role, Role):
+            role = role.id
+
+        async with self.bot.pool.acquire() as database:
+            await database.execute(
+                "INSERT INTO public.subscribe (guild_id, role_id) VALUES ($1, $2)",
+                ctx.guild.id,
+                role
+            )
+
+        await ctx.send(f"The role has been Successfully to {role_id.mention}")
+
+
+def setup(bot: Bot) -> None:
+    """Load the Common cog."""
+    bot.add_cog(Common(bot))
